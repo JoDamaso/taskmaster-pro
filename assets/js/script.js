d0bd05c9eee80543c32de596795cc15de72f625d
JoDamaso <darryldamaso@gmail.com>
2022-01-25 00:34:01 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,14 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time,"days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  // console.log(taskEl);
 };
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
 
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
@@ -80,7 +87,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -207,16 +214,22 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
+    // console.log("activate", this);
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
+    // console.log("deactivate", this);
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
+    // console.log("over", event.target);
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active")
+    // console.log("out", event.target);
   },
   update: function (event) {
     //array to store the task data in
@@ -256,14 +269,17 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active")
     ui.draggable.remove();
-    console.log("drop");
+    // console.log("drop");
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
+    // console.log("over");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
+    // console.log("out");
   }
 });
 
